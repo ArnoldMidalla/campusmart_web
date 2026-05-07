f5b26bb45dbb82b7badbef82677d28b5fe0e2332
Favour Akande <favourakande1@gmail.com>
2026-05-07 16:16:04 +0100

    Corrected prisma ORM setup

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,38 +1,31 @@
+import "dotenv/config";
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
-
+const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL!});
+const prisma = new PrismaClient({ adapter });
 
 @Injectable()
 export class PrismaService  implements OnModuleInit, OnModuleDestroy {
-    private readonly client: PrismaClient;
-    constructor() {
-        const pool = new Pool({
-            connectionString: process.env.DATABASE_URL!,
-        });
-        const adapter = new PrismaPg(pool);
-        this.client = new PrismaClient({ adapter });
-    }
 
-    get user() { return this.client.user}
-    get listing() { return this.client.listing; }
-    get listingImage() { return this.client.listingImage; }
-    get order() { return this.client.order; }
-    get orderItem() { return this.client.orderItem; }
-    get payment() { return this.client.payment; }
-    get review() { return this.client.review; }
-    get wishlist() { return this.client.wishlist; }
-    get institution() { return this.client.institution; }
-    get verificationRequest() { return this.client.verificationRequest; }
-    get moderationAction() { return this.client.moderationAction; }
+    get user() { return prisma.user}
+    get listing() { return prisma.listing; }
+    get listingImage() { return prisma.listingImage; }
+    get order() { return prisma.order; }
+    get orderItem() { return prisma.orderItem; }
+    get payment() { return prisma.payment; }
+    get review() { return prisma.review; }
+    get wishlist() { return prisma.wishlist; }
+    get institution() { return prisma.institution; }
+    get verificationRequest() { return prisma.verificationRequest; }
+    get moderationAction() { return prisma.moderationAction; }
     
     async onModuleInit() {
-        await this.client.$connect();
+        await prisma.$connect();
     }
 
     async onModuleDestroy() {
-        await this.client.$disconnect();
+        await prisma.$disconnect();
     }
 }
