9c12026470b6aa1da7e04a7244ac2aeb78c017ad
Favour Akande <favourakande1@gmail.com>
2026-05-07 14:25:14 +0100

    Moved pool to constructor

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,14 +3,15 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL!,
-});
+
 
 @Injectable()
 export class PrismaService  implements OnModuleInit, OnModuleDestroy {
     private readonly client: PrismaClient;
     constructor() {
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL!,
+        });
         const adapter = new PrismaPg(pool);
         this.client = new PrismaClient({ adapter });
     }
